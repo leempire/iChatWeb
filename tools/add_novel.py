f5bbbf1f2000a5a28c38a6f93528c77556f9de87
leempire <duorui_li@sjtu.edu.cn>
2024-12-02 16:31:33 +0800

    新增tools,提供一些脚本

--- tools/add_novel.py
+++ tools/add_novel.py
@@ -1,4 +1,11 @@
 import os
+import sys
+
+# 获取项目根目录的路径
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# 将项目根目录添加到系统路径
+sys.path.append(project_root)
+
 from app.base import sql_manager
 
 
